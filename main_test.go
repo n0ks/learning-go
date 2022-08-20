1ff3cdc7c7e34e5c36f9fd6233836fac21057fba
n0ks <rodrigocmp29@gmail.com>
2022-08-19 22:46:22 -0300

    test: talk in spanish 🌯

--- main_test.go
+++ main_test.go
@@ -5,15 +5,23 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run("say hi to people", func(t *testing.T) {
-		got := Hi("John")
+		got := Hi("John", "")
 		want := "Hi, John"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hi, fuck you' when empty string is provided", func(t *testing.T) {
-		got := Hi("")
+		got := Hi("", "")
 		want := "Hi, fuck you"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("talk in sPaNiSh", func(t *testing.T) {
+
+		got := Hi("Cucaracha", "Spanish")
+		want := "Hola, Cucaracha"
+
 		assertCorrectMessage(t, got, want)
 	})
 
