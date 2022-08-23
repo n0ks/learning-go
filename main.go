e4b12a47341409cb0affe2b5ccf4a5996cec69a4
n0ks <rodrigocmp29@gmail.com>
2022-08-23 20:22:40 -0300

    chore: remove main code

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
-package hello
+package main
 
 import "fmt"
 
-const prefix = "Hi, "
-const spanishPrefix = "Hola, "
-const spanish = "Spanish"
-
-func Hi(name string, lang string) string {
-
-	if name == "" {
-		return prefix + "fuck you"
-	}
-
-	switch lang {
-	case spanish:
-		return spanishPrefix + name
-	}
-
-	return prefix + name
-
-}
-
 func main() {
-	fmt.Println(Hi("John", ""))
+	fmt.Println("")
 }
